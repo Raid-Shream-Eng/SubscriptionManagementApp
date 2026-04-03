071a2307af2b53f7e87bb2dffc55480fd849baaf
Raid Shream Dev <Raidshreamdev@gmail.com>
2026-04-03 19:19:57 +0300

    fresh start

---
+++ app/(auth)/sing-in.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const singIn = () => {
+  return (
+    <View>
+      <Text>sing-In</Text>
+    </View>
+  );
+};
+
+export default singIn;
---
+++ constants/data.ts
@@ -0,0 +1,8 @@
+import { icons } from "./icons";
+
+export const tabs = [
+  { name: "index", titel: "Home", icon: icons.home },
+  { name: "subscriptions", titel: "Subscriptions", icon: icons.wallet },
+  { name: "insights", titel: "Insights", icon: icons.activity },
+  { name: "settings", titel: "Settings", icon: icons.setting },
+];
